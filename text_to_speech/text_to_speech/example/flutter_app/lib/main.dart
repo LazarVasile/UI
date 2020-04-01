1c7591a00bd30f2dbc44269e7ae928dd8ddbff80
Anghel Cristi <48655778+AnghelCristi@users.noreply.github.com>
2020-04-01 14:44:31 +0300

    t2s

---
+++ text_to_speech/text_to_speech/example/flutter_app/lib/main.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() => runApp(TheApp());
+
+class TheApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home:Scaffold(
+        body: MyApp(),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget{
+
+  final FlutterTts flutterTts = FlutterTts();
+
+  @override
+  Widget build(BuildContext context){
+    _speak() async{
+      await flutterTts.setLanguage("ro-RO");
+      await flutterTts.speak("ceva");
+    }
+    return Container(
+      alignment:Alignment.center,
+      child:RaisedButton(
+        child:Text("Apasa"),
+        onPressed: ()=> _speak(),
+      ),
+    );
+  }
+}
