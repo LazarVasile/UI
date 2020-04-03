1b41edae5bf6f1ae908a183508e3d1990122bbf6
Anghel Cristi <48655778+AnghelCristi@users.noreply.github.com>
2020-04-03 19:31:45 +0300

    up

--- text_to_speech/text_to_speech/example/flutter_app/lib/main.dart
+++ text_to_speech/text_to_speech/example/flutter_app/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     _speak() async{
       await flutterTts.setLanguage("ro-RO");
-      await flutterTts.speak("ceva");
+      await flutterTts.speak("Moara cu noroc este o nuvelă scrisă de Ioan Slavici, tratând consecințele pe care dorința de îmbogățire le are asupra destinului uman. Majoritatea criticilor o consideră creația cea mai importantă a autorului, alături de Pădureanca și romanul Mara. ");
     }
     return Container(
       alignment:Alignment.center,
